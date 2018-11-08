6056dcc76e461a7397c6405571d8cf6f76ebdccb
Uwe L. Korn <uwelk@xhochy.com>
2018-11-08 16:48:21 +0100

    Ensure correct linker on Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Otherwise this picks up the wrong linker on Linux (in most cases the system C++ compiler)
+if [ $(uname) == Linux ]; then
+  export LDSHARED="$CC -shared -pthread"
+fi
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
